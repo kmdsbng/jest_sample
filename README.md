b01f4370df1c4ef18c97d023c7b6da622ac16083
Yoshihiro Kameda <kameda.sbng@gmail.com>
2017-08-12 19:40:16 +0900

    add coverage

---
+++ README.md
@@ -0,0 +1,19 @@
+Jest sample
+
+
+```
+npm init
+npm install --save-dev jest @types/jest ts-jest typescript
+```
+
+* edit tsconfig.json
+* edit jest section in package.json
+
+```
+$(npm bin)/jest
+$(npm bin)/jest --coverage
+```
+
+
+
+
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -2,5 +2,6 @@ import {fizzBuzz} from "../src/fizzbuzz";
 test("FizzBuzz test", () =>{
   expect(fizzBuzz(2)).toBe("1 2 ");
   expect(fizzBuzz(3)).toBe("1 2 Fizz ");
+  expect(fizzBuzz(15)).toBe("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 Fizz Buzz ");
 });
 
